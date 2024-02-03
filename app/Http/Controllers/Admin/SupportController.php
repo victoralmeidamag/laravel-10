b3e38b06aadc3e89abe6405cf481a4c4bf970c84
magalhaes.victor <magalhaes.vctor@hcosta.com.br>
2024-02-03 15:55:42 -0300

    ajustes

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(){
-        return view('admin.supports.index');
+    public function index(Support $supports){
+
+        $supports = $supports->all();
+        
+
+        return view('admin.supports.index', compact('supports'));
+    }
+
+    public function create(){
+        return view('admin.supports.create');
+    }
+
+    public function store(Request $request){
+        $data = $request->all();
+        $date['status'] = 'a';
+
+        $support = Support::create($data);
+        return redirect()->route('supports.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use App\Http\Controllers\Admin\{SupportController};
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/suporte',[SupportController::class, 'index'])->name('suporte.index');
+Route::post('/supports',[SupportController::class,'store'])->name('supports.store');
+
+Route::get('/suporte/create',[SupportController::class, 'create'])->name('supports.create');
+
+Route::get('/suporte',[SupportController::class, 'index'])->name('supports.index');
 
 Route::get('/contato', [SiteController::class, 'contact']);
 
