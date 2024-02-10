b80b670f5fbf1784d333e2adfaa7f76d5b658347
victoralmeidamag <victorhugo.almeidamag@gmail.com>
2024-02-09 21:00:59 -0300

    ajustes

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -12,12 +12,12 @@
     <tbody>
     @foreach($supports as $support)
         <tr>
-            <td>{{ $support->subject }}</td>
-            <td>{{ $support->status === 'a' ? 'ATIVO': ($support->status === 'p' ? 'PENDENTE': 'CANCELADO')}}</td>
-            <td>{{ $support->body }}</td>
+            <td>{{ $support['subject'] }}</td>
+            <td>{{ $support['status'] === 'a' ? 'ATIVO': ($support['status'] === 'p' ? 'PENDENTE': 'CANCELADO')}}</td>
+            <td>{{ $support['body'] }}</td>
             <td>
-                <a href="{{ route('supports.show', $support->id)}}">Ir</a>
-                <a href="{{ route('supports.edit', $support->id)}}">Editar</a>
+                <a href="{{ route('supports.show', $support['id'])}}">Ir</a>
+                <a href="{{ route('supports.edit', $support['id'])}}">Editar</a>
         </td>
         </tr>
     @endforeach
